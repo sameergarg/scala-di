cc7702588c83762fadcc69aadb1262707a64f015
samirgarg <samir.garg@deloitte.co.uk>
2019-02-21 11:54:25 +0000

    DI using reader monad

--- build.sbt
+++ build.sbt
@@ -5,3 +5,4 @@ version := "0.1"
 scalaVersion := "2.12.8"
 
 //libraryDependencies +="com.google.inject" % "guice" % "3.0"
+libraryDependencies += "org.typelevel" %% "cats-core" % "1.6.0"
---
+++ src/main/scala/ReaderDI.scala
@@ -0,0 +1,61 @@
+import ReaderDI._
+import cats.data.Reader
+
+object ReaderDI {
+
+  case class Course(code: String, name: String)
+
+  //Individual service / Dependencies
+  class AuthService {
+    def isAuthorised(userName: String): Boolean = userName.startsWith("J")
+  }
+
+  //Individual service / Dependencies
+  class CourseService {
+    def register(course: Course, userName: String, isAuthorised: Boolean) = {
+      if (isAuthorised)
+        s"User $userName registered for the course: ${course.code}"
+      else
+        s"User: $userName is not authorised to register for course: ${course.code}"
+    }
+  }
+
+  //a.k.a Application Context
+  case class Environment( authService: AuthService,
+                          courseService: CourseService)
+
+
+
+  //Algebra
+  class CourseManagerReaderOps {
+    def isAuthorised(userName: String) = Reader[Environment, Boolean]{ courseMgr =>
+      courseMgr.authService.isAuthorised(userName)
+    }
+
+    def register(course: Course, userName: String, isFull: Boolean) = Reader[Environment, String] { courseMgr =>
+      courseMgr.courseService.register(course, userName,  isFull)
+    }
+  }
+
+  //DI using reader - Program
+  class CourseManager extends CourseManagerReaderOps {
+    def registerWhenAuthorised(course: Course,
+                              userName: String): Reader[Environment, String] = {
+      for {
+        authorised <- isAuthorised(userName)
+        response   <- register(course, userName, authorised)
+      } yield response
+    }
+  }
+}
+
+object Main extends App {
+  val course = Course("Computer Science", "CS")
+  val env = new Environment(new AuthService, new CourseService)
+
+  val register1 = new CourseManager().registerWhenAuthorised(course, "Jack")
+  val register2 = new CourseManager().registerWhenAuthorised(course, "Sam")
+
+  println(register1.run(env))
+  println(register2.run(env))
+}
